3f18abe4b9bb88deb1e9eed09f924c0772e19369
Tron <traminhtrong2604@gmail.com>
2024-09-20 12:11:06 +0700

    build: score management system

--- Assets/Planet.cs
+++ Assets/Planet.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class Planet : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+            
     }
 
     // Update is called once per frame
