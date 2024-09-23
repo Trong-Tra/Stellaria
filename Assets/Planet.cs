02994b565a8cd64c83ccec26c486a306cf554db2
Tron <trongtrawork@gmail.com>
2024-09-24 00:36:05 +0700

    build: trigger and collision logic

--- Assets/Planet.cs
+++ Assets/Planet.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class Planet : MonoBehaviour
 {
     public int id;
-    private GameManager game;
+    private GameManager gameManager;
     private bool hasCollided = false;
     public Handler handler;
     public bool isTouchingTrigger = false;
     void Start()
     {
-        game = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         handler = GameObject.Find("Handler").GetComponent<Handler>();
     }
 
@@ -28,7 +28,38 @@ public class Planet : MonoBehaviour
         yield return new WaitForSeconds(5f);
         if(isTouchingTrigger == true)
         {
-            game.endGame();
+            gameManager.endGame();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        handler.currentPlanet = null;
+        handler.SpawnNewPlanet();
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        isTouchingTrigger = true;
+        StartCoroutine(WaitForEndGame());
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+       isTouchingTrigger = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Planet otherPlanet = collision.gameObject.GetComponent<Planet>();
+        if (otherPlanet != null && 
+            otherPlanet.id == this.id && 
+            this.gameObject.GetInstanceID() < collision.gameObject.GetInstanceID()) 
+        {
+            gameManager.CreatePlanet(this.transform.position, this.id + 1);
+            gameManager.addScore(this.id);
+            StartCoroutine(DestroyPlanets(this.gameObject, collision.gameObject));
+            
         }
     }
 }
