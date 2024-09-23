939acc17689356a8abf852a188b9d096ecbed7b5
Tron <trongtrawork@gmail.com>
2024-09-23 23:54:12 +0700

    build: add temp solution to handler Update() function

--- Assets/Handler.cs
+++ Assets/Handler.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Handler : MonoBehaviour
 {
@@ -22,5 +23,31 @@ public class Handler : MonoBehaviour
         currentPlanet = gameManager.CreatePlanet(this.transform.position, randomPlanet);
 
         currentPlanet.GetComponent<Rigidbody2D>().gravityScale = 0;
+    }
+
+    private void Update()
+    {
+        /**
+         * @dev this is temporary solution and var
+         * @todo design game and fix var
+         * @todo developing new handler logic
+         */
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (!gameManager.gameOver && mousePosition.x < 2f && mousePosition.x > -2f)
+        {
+            mousePosition.z = 0;
+            mousePosition.y = 0;
+            transform.position = mousePosition;
+        }
+
+        if (currentPlanet != null && currentPlanet.GetComponent<Rigidbody2D>().gravityScale == 0)
+        {
+            currentPlanet.transform.position = this.transform.position;
+        }
+
+        if (!gameManager.gameOver && Input.GetMouseButton(0))
+        {
+            currentPlanet.GetComponent<Rigidbody2D>().gravityScale = -1;
+        }
     }
 }
