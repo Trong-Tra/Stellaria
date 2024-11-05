29be94e3b5e8b6e245664cac7acdd0394a7b157d
Tron <trongtrawork@gmail.com>
2024-11-05 11:02:00 +0700

    fix: trigger logic is now work as expected

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -5,8 +5,8 @@ public class Planet : MonoBehaviour
 {
     public int id;
     private GameManager gameManager;
-    private bool hasCollided = false;
     public Handler handler;
+    private bool hasCollided = false;
     public bool isTouchingTrigger = false;
     void Start()
     {
@@ -32,32 +32,53 @@ public class Planet : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        handler.currentPlanet = null;
-        handler.SpawnNewPlanet();
+        if (GetComponent<Rigidbody2D>().gravityScale != 0 && !hasCollided)
+        {
+            hasCollided = true;
+            if (handler != null)
+            {
+                handler.ReadyForNewPlanet();
+            }
+            else
+            {
+                Debug.LogError("Handler reference is null!");
+            }
+        }
     }
 
     void OnTriggerStay2D(Collider2D collision)
     {
-        isTouchingTrigger = true;
-        StartCoroutine(WaitForEndGame());
+        if (!isTouchingTrigger)
+        {
+            isTouchingTrigger = true;
+            StartCoroutine(WaitForEndGame());
+        }
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
        isTouchingTrigger = false;
+       StopAllCoroutines();
     }
 
+    /**
+     * @dev new planet after merge will transform in the middle of 2 planets that collided
+     * @note score increase after a successful collided (2 planets have the same id handle in if condition)
+     */
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Planet otherPlanet = collision.gameObject.GetComponent<Planet>();
         if (otherPlanet != null && 
             otherPlanet.id == this.id && 
+            !gameManager.gameOver &&
             this.gameObject.GetInstanceID() < collision.gameObject.GetInstanceID()) 
         {
-            gameManager.CreatePlanet(this.transform.position, this.id + 1);
+            Vector3 midPoint = (transform.position + collision.transform.position) / 2f;
+            GameObject newPlanet = gameManager.CreatePlanet(midPoint, this.id + 1);
+            newPlanet.GetComponent<Rigidbody2D>().gravityScale = -1; // reverse gravity for new planet
+
             gameManager.addScore(this.id);
-            StartCoroutine(DestroyPlanets(this.gameObject, collision.gameObject));
-            
+            StartCoroutine(DestroyPlanets(this.gameObject, collision.gameObject)); // destroy 2 collided planets
         }
     }
 }
