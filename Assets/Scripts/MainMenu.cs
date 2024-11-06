4cfbd780ff6de97322bd76e98899fba03f2df672
Minh Pham <giaminh2042004@gmail.com>
2024-11-07 06:32:20 +0700

    fix: typing error in MainMenu script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScenceAsync(1);
+        SceneManager.LoadSceneAsync(1);
     }
 
     public void QuitGame()
